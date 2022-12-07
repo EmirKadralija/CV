03a978c925fc8144e9a12ccb0a4455845b28f8c3
Emir Kadralija <43282886+EmirKadralija@users.noreply.github.com>
2022-12-07 14:10:13 +0000

    Update ajs.js

--- ajs.js
+++ ajs.js
@@ -1,9 +1,8 @@
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156668371-3"></script>
-<script>
+async src="https://www.googletagmanager.com/gtag/js?id=UA-156668371-3"
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-156668371-3');
-</script>
